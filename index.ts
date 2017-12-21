3650cddbeec0775daec12ee6c5e5fb28ca704223
andraaspar <andraaspar@gmail.com>
2017-12-21 15:36:04 +0100

    Implemented fene.

--- index.ts
+++ index.ts
@@ -2,3 +2,13 @@
 export function feneValue(length: number, index: number): boolean {
 	return !(Math.floor(index / length) % 2)
 }
+
+export function fene(patternLength: number, length: number, o?: { invert?: boolean }): boolean[] {
+	let result: boolean[] = []
+	for (let i = 0; i < length; i++) {
+		let value = feneValue(patternLength, i)
+		if (o && o.invert) value = !value 
+		result.push(value)
+	}
+	return result
+}
--- tests.ts
+++ tests.ts
@@ -1,5 +1,5 @@
 import { test } from 'ava'
-import { feneValue } from './index'
+import { feneValue, fene } from './index'
 
 test('feneValue of length 1 goes like [x x x x x ]', t => {
 	let result = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => feneValue(1, i))
@@ -17,4 +17,28 @@ test('feneValue of length 3 goes like [xxx   xxx ]', t => {
 	let result = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(i => feneValue(3, i))
 	// console.log(result)
 	t.deepEqual(result, [true, true, true, false, false, false, true, true, true, false])
+})
+
+test('fene of patternLength 1 goes like [x x x x x ]', t => {
+	let result = fene(1, 10)
+	// console.log(result)
+	t.deepEqual(result, [true, false, true, false, true, false, true, false, true, false])
+})
+
+test('fene of patternLength 2 goes like [xx  xx  xx]', t => {
+	let result = fene(2, 10)
+	// console.log(result)
+	t.deepEqual(result, [true, true, false, false, true, true, false, false, true, true])
+})
+
+test('fene of patternLength 3 goes like [xxx   xxx ]', t => {
+	let result = fene(3, 10)
+	// console.log(result)
+	t.deepEqual(result, [true, true, true, false, false, false, true, true, true, false])
+})
+
+test('fene of patternLength 1 & inverted goes like [ x x x x x]', t => {
+	let result = fene(1, 10, { invert: true })
+	// console.log(result)
+	t.deepEqual(result, [false, true, false, true, false, true, false, true, false, true])
 })
